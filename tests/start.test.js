3fbd2c06a8b3406b0f92dd6e924e18121aa945f7
samuelmayna <samuelmayna@gmail.com>
2021-07-26 22:05:41 +0300

    Added code to alter, insert and delete data into an sql table

--- tests/start.test.js
+++ tests/start.test.js
@@ -12,10 +12,14 @@ const password = '';
 
 const database = 'people';
 
+const globalCb = err => {
+	if (err) throw new Error(err);
+};
+
 describe('starting tests', () => {
 	let connection;
 	beforeAll(done => {
-		const cbWithDone = err => {
+		const cb = err => {
 			if (err) throw new Error(err);
 			done();
 		};
@@ -26,13 +30,20 @@ describe('starting tests', () => {
 			database,
 		});
 
-		con.connect(cbWithDone);
+		con.connect(cb);
 		connection = con;
+	});
+	afterAll(done => {
+		connection.end();
+		done();
 	});
 	it(
 		'create table',
 		done => {
-			const create = `CREATE TABLE trial(
+			const name = 'trial';
+
+			const create = `CREATE TABLE ${name}
+			(
             dept_name VARCHAR(20) NOT NULL,
             faculty VARCHAR(20) NOT NULL,
             budget NUMERIC(12,2),
@@ -40,92 +51,98 @@ describe('starting tests', () => {
             num_of_employees INT(20),
             PRIMARY KEY (dept_name,faculty)
         )`;
-			let cb = err => {
-				if (err) throw new Error(err);
-			};
-			connection.query(create, cb);
-			cb = err => {
-				if (err) throw new Error(err);
-				done();
-			};
-			dropTable('trial', cb);
+			connection.query(create, globalCb);
+			dropTable(name, globalCb);
+			done();
 		},
 		MAX_TESTING_TIME
 	);
 	it(
 		'delete table',
 		done => {
-			const table = 'department';
-			const schema = ` dept_name VARCHAR(20) NOT NULL,
-            faculty VARCHAR(20) NOT NULL,
-            budget NUMERIC(12,2),
-            type_serial_no CHAR(10) NOT NULL,
-            num_of_employees INT(20),
-            PRIMARY KEY (dept_name,faculty)`;
-			createTable(table, schema, () => undefined);
+			const table = {
+				name: 'department',
+				schema: `dept_name VARCHAR(20) NOT NULL,
+                         faculty VARCHAR(20) NOT NULL,
+                         budget NUMERIC(12,2),
+                         type_serial_no CHAR(10) NOT NULL,
+                         num_of_employees INT(20),
+                         PRIMARY KEY (dept_name,faculty)`,
+			};
+
+			createTable(table, globalCb);
+
 			cb = err => {
 				if (err) throw new Error(err);
 				done();
 			};
-			connection.query(`DROP TABLE ${table}`, cb);
+			connection.query(`DROP TABLE ${table.name}`, cb);
 		},
 		MAX_TESTING_TIME
 	);
 	describe('alter table schema', () => {
-		const table = 'department';
-		beforeEach(done => {
-			const cbWithDone = err => {
-				if (err) throw new Error(err);
-				done();
-			};
-			const schema = ` dept_name VARCHAR(20) NOT NULL,
+		const table = {
+			name: 'departments',
+			schema: ` dept_name VARCHAR(20) NOT NULL,
             faculty VARCHAR(20) NOT NULL,
             budget NUMERIC(12,2),
             type_serial_no CHAR(10) NOT NULL,
             num_of_employees INT(20),
-            PRIMARY KEY (dept_name,faculty)`;
-			createTable(table, schema, cbWithDone);
-		});
-		afterEach(done => {
-			const cbWithDone = err => {
-				if (err) throw new Error(err);
-				done();
-			};
-			dropTable(table, cbWithDone);
-		});
-		it('adding attributes', done => {
-			cb = err => {
-				if (err) throw new Error(err);
-				done();
-			};
-			connection.query(`ALTER TABLE ${table} ADD vision VARCHAR(50)`, cb);
-		});
-		it('removing attributes', done => {
-			const field = 'num_of_employees';
-			cb = err => {
-				if (err) throw new Error(err);
-				done();
-			};
-			connection.query(`ALTER TABLE ${table} DROP ${field}  `, cb);
-		});
-	});
-
-	describe('insert into tables', () => {
-		const tableName = 'department';
-
+            PRIMARY KEY (dept_name,faculty)`,
+		};
 		beforeEach(done => {
-			const schema = `dept_name VARCHAR(20),
-                        building VARCHAR(20),
-                        budget NUMERIC(12,2),
-                        PRIMARY KEY(dept_name)`;
-			createTable(tableName, schema, done);
+			const cb = err => {
+				if (err) throw new Error(err);
+				done();
+			};
+
+			createTable(table, cb);
 		});
 		afterEach(done => {
 			const cb = err => {
 				if (err) throw new Error(err);
 				done();
 			};
-			dropTable(tableName, cb);
+			dropTable(table.name, cb);
+		});
+		it('adding attributes', done => {
+			const cb = err => {
+				if (err) throw new Error(err);
+				done();
+			};
+			connection.query(`ALTER TABLE ${table.name} ADD vision VARCHAR(50)`, cb);
+		});
+		it('removing attributes', done => {
+			const field = 'num_of_employees';
+			const cb = err => {
+				if (err) throw new Error(err);
+				done();
+			};
+			connection.query(`ALTER TABLE ${table.name} DROP ${field}  `, cb);
+		});
+	});
+
+	describe('insert into tables', () => {
+		const table = {
+			name: 'department',
+			schema: `dept_name VARCHAR(20),
+                        building VARCHAR(20),
+                        budget NUMERIC(12,2),
+                        PRIMARY KEY(dept_name)`,
+		};
+		beforeEach(done => {
+			const cb = err => {
+				if (err) throw new Error(err);
+				done();
+			};
+			createTable(table, cb);
+		});
+		afterEach(done => {
+			const cb = err => {
+				if (err) throw new Error(err);
+				done();
+			};
+			dropTable(table.name, cb);
 		});
 		it(
 			'inserting data in bulk',
@@ -135,13 +152,12 @@ describe('starting tests', () => {
 					building: 'Prof Ken',
 					budget: 1345.68,
 				};
-				const insert = `INSERT INTO ${tableName} SET ?`;
-				cb = err => {
-					if (err) throw new Error(err);
-				};
-				connection.query(insert, department1, cb);
-				const fetchAll = `SELECT * FROM ${tableName}`;
-				cb = (err, results) => {
+				const { name } = table;
+				const insert = `INSERT INTO ${name} SET ?`;
+
+				connection.query(insert, department1, globalCb);
+				const fetchAll = `SELECT * FROM ${name}`;
+				const cb = (err, results) => {
 					if (err) throw new Error(err);
 					for (const result of results) {
 						expect(result.dept_name).toBe(department1.dept_name);
@@ -156,50 +172,51 @@ describe('starting tests', () => {
 		);
 	});
 
-	describe.skip('delete data from table', () => {
-		const tableName = 'department';
+	describe('delete data from table', () => {
+		const table = {
+			name: 'department',
+			schema: `dept_name VARCHAR(20),
+                     building VARCHAR(20),
+                     budget NUMERIC(12,2),
+                     PRIMARY KEY(dept_name)
+                    `,
+		};
 		const TRIALS = 20;
 		beforeEach(done => {
-			const schema = `dept_name VARCHAR(20),
-                        building VARCHAR(20),
-                        budget NUMERIC(12,2),
-                        PRIMARY KEY(dept_name)`;
-			createTable(tableName, schema, () => undefined);
+			createTable(table, globalCb);
 
-			let cb = err => {
-				if (err) throw new Error(err);
-			};
 			for (let i = 0; i < TRIALS; i++) {
 				const department1 = {
 					dept_name: `Math ${i}`,
 					building: `Prof Ken ${i}`,
 					budget: (Math.random() * TRIALS).toFixed(2),
 				};
-				const insert = `INSERT INTO ${tableName} SET ?`;
-				connection.query(insert, department1, cb);
+				const insert = `INSERT INTO ${table.name} SET ?`;
+				connection.query(insert, department1, globalCb);
 			}
 			done();
 		});
-		afterEach(() => {
-			dropTable(tableName);
+		afterEach(done => {
+			const cb = err => {
+				if (err) throw new Error(err);
+				done();
+			};
+			dropTable(table.name, cb);
 		});
 		it(
-			'',
+			'delete data for one field',
 			done => {
-				let cb = err => {
-					if (err) {
-						throw new Error(err);
-					}
-				};
-
+				const { name } = table;
 				for (let i = 0; i < TRIALS; i++) {
-					const del = `DELETE FROM ${tableName} WHERE dept_name= 'Math ${i}'`;
-					connection.query(del, cb);
+					const del = `DELETE FROM ${name} WHERE dept_name= 'Math ${i}'`;
+					connection.query(del, globalCb);
 				}
 
-				const fetchAll = `SELECT * FROM ${tableName}`;
+				const fetchAll = `SELECT * FROM ${name}`;
 				cb = (err, results) => {
 					if (err) throw new Error(err);
+
+					//we have deleted every thing so we expect to have nothing in the database.
 					expect(results.length).toBe(0);
 					done();
 				};
@@ -209,7 +226,8 @@ describe('starting tests', () => {
 		);
 	});
 
-	const createTable = (name, schema, cb) => {
+	const createTable = (config, cb) => {
+		const { name, schema } = config;
 		const create = `CREATE TABLE ${name}(${schema})`;
 		connection.query(create, cb);
 	};
